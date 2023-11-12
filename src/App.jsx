6da70086c96e2603ee8b321caa9b1a9d063893b6
vinayak-naik <vinayaknaik2021@gmail.com>
2023-11-12 18:09:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>Layout Micro Frontend</div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+      react(),
+      federation({
+        name: 'remote-app',
+        filename: 'remoteEntry.js',
+        // Modules to expose
+        exposes: {
+          './App': './src/App.jsx',
+        },
+        shared: ['react','react-dom']
+      })
+  ],
+  build: {
+    target: 'esnext'
+  },
+})
